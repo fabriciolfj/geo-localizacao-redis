763d87866971ebb06bf6bad65397340bc9455a3c
spark <fabricio.jacob@outlook.com>
2025-04-12 20:04:24 -0300

    feat: init project

---
+++ src/main/kotlin/com/github/fabriciolfj/geo_localizacao/GeoLocalizacaoApplication.kt
@@ -0,0 +1,11 @@
+package com.github.fabriciolfj.geo_localizacao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeoLocalizacaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GeoLocalizacaoApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/fabriciolfj/geo_localizacao/GeoLocalizacaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.geo_localizacao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeoLocalizacaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
